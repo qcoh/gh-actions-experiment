a8bedffa17c2ccaf7608ab394785a9dfc46756b2
Jeremias Yehdegho <j.yehdegho@gmail.com>
2020-06-27 13:46:07 +0200

    add simple fibonacci package

---
+++ internal/fibonacci/fibonacci.go
@@ -0,0 +1,14 @@
+package fibonacci
+
+// Fibonacci computes the nth Fibonacci number.
+//
+// No care is being taken about integer overflows.
+func Fibonacci(n int) int {
+	a, b := 0, 1
+
+	for ; n > 0; n-- {
+		a, b = b, a+b
+	}
+
+	return a
+}
